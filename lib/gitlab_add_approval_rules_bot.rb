61c13c616597e471e8ca79e3b88c74ae64742f0f
Shoma FUKUDA <fkshom@gmail.com>
2022-07-15 00:21:20 +0900

    wip

--- lib/gitlab_add_approval_rules_bot.rb
+++ lib/gitlab_add_approval_rules_bot.rb
@@ -8,7 +8,7 @@ require "dotenv"
 Dotenv.load
 
 Gitlab.configure do |config|
-  config.endpoint       = ENV['GITLAB_ENDPOINT'] or ENV['CI_API_V4_URL']
+  config.endpoint       = ENV['GITLAB_ENDPOINT'] || ENV['CI_API_V4_URL']
   config.private_token  = ENV['GITLAB_TOKEN']
 end
 
@@ -21,65 +21,66 @@ module GitlabAddApprovalRulesBot
 
     desc "apply", "apply"
     option :dry_run, aliases: "-d", type: :boolean, desc: "dry run mode", default: false
-    def apply(config_filename=nil)
+    option :config, aliases: "-c", type: :string, desc: "config file name", default: "config.yml"
+    def apply(*rulenames)
       config_filename = "config.yml" if config_filename.nil?
       config = YAML.load_file(config_filename)
-      project_id = ENV['PROJECT_ID'] or ENV['CI_PROJECT_ID']
-      merge_request_iid = ENV['MERGE_REQUEST_IID'] or ENV['CI_MERGE_REQUEST_IID']
+      project_id = ENV['PROJECT_ID'] || ENV['CI_PROJECT_ID']
+      merge_request_iid = ENV['MERGE_REQUEST_IID'] || ENV['CI_MERGE_REQUEST_IID']
 
-      config['rules'].each do |rule|
+      rulenames.each do |rulename|
+        if not config['rules'].key? rulename
+          puts "SKIP: rulename #{rulename} in not exists."
+          next
+        end
+        rule = config['rules'][rulename]
         rule_options = {
           name: rule['name'],
           approvals_required: rule['approvals_required'],
           user_ids: [],
           group_ids: [],
         }
-        if rule.has_key? 'approval_project_rule_id'
-          rule_options[:approval_project_rule_id] = rule['approval_project_rule_id']
-        end
-        if rule.has_key? 'user_ids'
-          rule_options[:user_ids] = rule['user_ids']
-        end
-        if rule.has_key? 'group_ids'
-          rule_options[:group_ids] = rule['group_ids']
-        end
-        if rule.has_key? 'usernames'
-          rule['usernames'].each do |username|
-            user = Gitlab.user_search(username)&.first
-            pp user
-            if not user.nil? and user['username'] == username
-              rule_options[:user_ids] << user['id'] unless user.nil?
-            end
-          end
-        end
-        if rule.has_key? 'groupnames'
-          rule['groupnames'].each do |groupname|
-            group = Gitlab.group_search(groupname)&.first
-            pp group
-            if not group.nil? and group['groupname'] == groupname
-              rule_options[:group_ids] << group['id'] unless group.nil?
-            end
 
+        rule_options[:approval_project_rule_id] = rule['approval_project_rule_id'] || nil
+        rule_options[:user_ids] = rule['user_ids'] || []
+        rule_options[:group_ids] = rule['group_ids'] || []
+
+        (rule['usernames'] || []).each do |username|
+          user = Gitlab.user_search(username)&.first
+          pp user
+          if not user.nil? and user['username'] == username.to_s
+            rule_options[:user_ids] << user['id']
           end
         end
+
+        (rule['groupnames'] || []).each do |groupname|
+          group = Gitlab.group_search(groupname)&.first
+          pp group
+          if not group.nil? and group['groupname'] == groupname.to_s
+            rule_options[:group_ids] << group['id']
+          end
+        end
+
         puts "project_id: #{project_id}"
         puts "merge_request_iid: #{merge_request_iid}"
         puts "options: #{rule_options}"
         if options[:dry_run]
           puts "THIS IS DRY RUN MODE!!"
-        else
-          if project_id.nil?
-            puts "project_id is nil"
-            puts "exit"
-            return
-          end
-          if merge_request_iid.nil?
-            puts "merge_request_iid is nil"
-            puts "exit"
-            return
-          end
-          Gitlab.create_merge_request_level_rule(project_id, merge_request_iid, rule_options)
+          next
         end
+
+        if project_id.nil?
+          puts "project_id is nil"
+          puts "exit"
+          next
+        end
+        if merge_request_iid.nil?
+          puts "merge_request_iid is nil"
+          puts "exit"
+          next
+        end
+
+        Gitlab.create_merge_request_level_rule(project_id, merge_request_iid, rule_options)
       end
     end
   end
